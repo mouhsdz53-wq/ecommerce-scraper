ab3248bcd91d75d18ad16c374df649a26450a331
mouhsdz53-wq <mouhsdz53@example.com>
2026-01-17 20:23:19 +0100

    Fix postcss.config.js syntax error

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,3 +1,7 @@
-@import 'tailwindcss/base';
-@import 'tailwindcss/components';
-@import 'tailwindcss/utilities';
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+}
+
